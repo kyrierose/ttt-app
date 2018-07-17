fc843e4b87083c6da5bace9391f2e453abf53f24
Akshit Dhar <akshitdhar@live.com>
2018-07-17 09:32:23 +0530

    Used promises to fetch data

--- backend/Routes/api.js
+++ backend/Routes/api.js
@@ -5,20 +5,6 @@ import { SSL_OP_NO_TLSv1_2 } from 'constants';
 //Initialise the router
 const router = express.Router()
 
-//Store data object
-let data = undefined, result = undefined
-
-//Fetching data asynchronously
-let async = () => {
-    //Fetches and returns the content of the file
-    request.get('http://terriblytinytales.com/test.txt', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            data = body
-        }
-    });
-}
-async()
-
 const topFrequent = function(string) {
     //converting string to words
     const words = string.split(' ')
@@ -30,29 +16,44 @@ const topFrequent = function(string) {
     return wc
 };
 
-//Wait until data is not empty
-function callback(num, res){
-    if(data === undefined)
-        setTimeout(callback(num, res), 1000)
-    console.log("Download data done")
-    let freqObj = topFrequent(data)
-    
-    let final_object = {}
-    
-    //Dumb code follows
-    for(let index=0;index<num;index++){
-        //get key for max object
-        let max_element_key = Object.keys(freqObj).reduce((a, b) => freqObj[a] > freqObj[b] ? a : b)
-        let max_element_value = freqObj[max_element_key]
-        //push these key-value to new object
-        final_object[max_element_key] = max_element_value
-        //remove max_element from freqObj
-        delete freqObj[max_element_key]
-    }
-    res.send(JSON.stringify(final_object))
+function intialize(){
+    return new Promise((resolve, reject)=>{
+        request.get('http://terriblytinytales.com/test.txt', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                resolve(body)
+            }
+            else
+                reject(err)
+        });
+    })
 }
+
+let downloaded_text
+let final_object = {}
+
 router.post('/:id', (req, res)=>{    
     const num = req.params.id
-    callback(num, res)
+    //callback(num, res)
+    var initializePromise = intialize();
+    initializePromise.then(function(result) {
+        downloaded_text = result;
+        
+        let freqObj = topFrequent(downloaded_text)
+        
+        //Dumb code follows
+        for(let index=0;index<num;index++){
+            //get key for max object
+            let max_element_key = Object.keys(freqObj).reduce((a, b) => freqObj[a] > freqObj[b] ? a : b)
+            let max_element_value = freqObj[max_element_key]
+            //push these key-value to new object
+            final_object[max_element_key] = max_element_value
+            //remove max_element from freqObj
+            delete freqObj[max_element_key]
+        }
+        res.send(JSON.stringify(final_object))
+    }, function(err) {
+        console.log(err);
+        res.send({msg: 'Error fetching the data.', status:500})
+    })
 })
 export default router
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ttt-app';
+  num=0
+  private url = "http://localhost:3000/api/"
+
+  constructor(private http:HttpClient){}
+
+  onSubmit(){
+    this.http.post(this.url,this.num).subscribe(
+      res=>console.log(res),
+      err=>console.log(err)
+    )
+  }
 }
